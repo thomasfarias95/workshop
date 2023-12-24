1f77a298fdd2a4de41c1fedd16b5320feb0f99c3
Thomas Farias <thomasfarias0995@gmail.com>
2023-12-24 14:14:06 -0300

    Entity user Rest fucinando

---
+++ src/main/java/com/workshop/demo/domain/User.java
@@ -0,0 +1,58 @@
+package com.workshop.demo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String id;
+    private String email;
+
+    public  User(){
+
+    }
+
+    public User(String name, String id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
